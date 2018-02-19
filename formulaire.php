91dbdcf3ce5d69ae4ed205e10e8e8e50f9536f91
geoffrey <canivetgeoffrey@gmail.com>
2018-02-19 10:56:02 +0100

    mise a jour html

---
+++ formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todolist</title>
+</head>
+<body>
+    <label>A FAIRE</label>
+    <form>
+    <input type="checkbox" name="choix1" value="choix1"> Choix 1<br>
+    <input type="checkbox" name="choix2" value="choix2"> Choix 2<br>
+    <input type="checkbox" name="choix3" value="choix3"> Choix 3<br>
+    <input type="submit" name="submit" value="Enregistrer">
+
+</body>
+</html>
